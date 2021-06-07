d426e537651ff6cc7cd219b436685bba19013fa3
Karsten Küpper <karsten.kuepper@bcc.no>
2021-06-07 20:17:34 +0200

    add comment

--- BMM.UI.Android/Application/Bindings/MvxCardCircle.cs
+++ BMM.UI.Android/Application/Bindings/MvxCardCircle.cs
@@ -4,6 +4,13 @@ using MvvmCross.Platforms.Android.Binding.Target;
 
 namespace BMM.UI.Droid.Application.Bindings
 {
+    /// <summary>
+    /// If the value is true, it transforms it into a circular / completely round image.
+    /// This has been implemented to implement the requirements for podcasts and contributors. However it falls short in other use cases.
+    /// E.g. it won't change back to non-circular if the value property changes to false.
+    /// Also if the CardView is really big it might only get rounded corners (I can't image such a big CardView though).
+    /// Therefore be cautious when using this.
+    /// </summary>
     public class MvxCardCircle : MvxAndroidTargetBinding
     {
         public MvxCardCircle(object target) : base(target)
