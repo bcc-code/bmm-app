08e20e998e979df003d196163c484dba26e2e49c
Arkadiusz Krol <arkadiusz.krol@rocksoft.pl>
2021-11-19 17:10:25 +0100

    Image Cache set to 30 days

--- BMM.Core/Constants/ImageServiceConstants.cs
+++ BMM.Core/Constants/ImageServiceConstants.cs
@@ -7,6 +7,6 @@ namespace BMM.Core.Constants
         public const string ImageCacheFolder = "img";
         public const int ImageCacheMemorySize = 100 * 1024 * 1024; //100 MB
 
-        public static readonly TimeSpan DiskCacheDuration = TimeSpan.FromDays(7);
+        public static readonly TimeSpan DiskCacheDuration = TimeSpan.FromDays(30);
     }
 }
