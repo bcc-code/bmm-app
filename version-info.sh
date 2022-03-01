c832b290bf21b9b29ff3daa8cd8ae786a2e41b50
Karsten Küpper <karsten.kuepper@bcc.no>
2022-03-01 14:42:45 -0500

    update version-info.sh

--- version-info.sh
+++ version-info.sh
@@ -5,7 +5,7 @@ branch=$(Build.SourceBranch)
 
 ## These two lines can be used to run the script locally
 #versionInfo="\"1.12.4.3.0.0\""
-#branch="refs/heads/master"
+#branch="refs/heads/main"
 
 # major.minor.patch.beta.alpha.hotfix-beta
 # develop -> 1.1.0-alpha1      1.0.0.0.1.0
@@ -13,13 +13,13 @@ branch=$(Build.SourceBranch)
 # release -> 1.1.0-beta1       1.0.0.1.2.0
 # develop -> 1.1.0-alpha3      1.0.0.1.3.0
 # release -> 1.1.0-beta2       1.0.0.2.3.0  (minor++, increment beta)
-# master  -> 1.1.0             1.1.0.0.0.0  (increment minor, reset patch, beta, alpha)    VV1
+# main    -> 1.1.0             1.1.0.0.0.0  (increment minor, reset patch, beta, alpha)    VV1
 # develop -> 1.2.0-alpha1      1.1.0.0.1.0  (minor++, increment alpha)
 # hotfix  -> 1.1.1-beta1       1.1.0.0.1.1  (patch++, increment hotfix-beta)
-# master  -> 1.1.1             1.1.1.0.1.0 (increment patch, reset hotfix-beta)      VV2
+# main    -> 1.1.1             1.1.1.0.1.0 (increment patch, reset hotfix-beta)      VV2
 # develop -> 1.2.0-alpha2      1.1.1.0.2.0 (minor++, increment alpha)
 # hotfix  -> 1.1.2-beta1       1.1.1.0.2.1 (patch++, increment hotfix-beta)
-# master  -> 1.1.2             1.1.2.0.2.0  (increment patch, reset hotfix-beta)      VV2
+# main    -> 1.1.2             1.1.2.0.2.0  (increment patch, reset hotfix-beta)      VV2
 
 echo "Parse version number for branch $branch: $versionInfo"
 
@@ -46,7 +46,7 @@ if [[ "$branch" == "refs/heads/release/"* ]]
     printPatch=0
     beta=$(($beta + 1))
     printRevision="-beta$beta"
-	hotfixbeta=0
+    hotfixbeta=0
 
 elif [[ "$branch" == "refs/heads/hotfix/"* ]]
   then
@@ -54,14 +54,14 @@ elif [[ "$branch" == "refs/heads/hotfix/"* ]]
     hotfixbeta=$(($hotfixbeta + 1))
     printRevision="-beta$hotfixbeta"
 
-elif [ "$branch" == "refs/heads/master" ]
+elif [ "$branch" == "refs/heads/main" ]
   then
     if [ "$hotfixbeta" -eq 0 ]
       then
         minor=$(($minor + 1))
         printMinor=$minor
         patch=0
-		printPatch=0
+        printPatch=0
         beta=0
         alpha=0
       else
