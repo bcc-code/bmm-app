a58bdf4e6d221884d0f715435fa94ddf325d5cfd
Arkadiusz Król <arkadiusz.krol@rocksoft.pl>
2022-06-15 21:05:47 +0200

    Android PendingIntent Immutable or Mutable flag needed issue fix (#204)

--- BMM.UI.Android/Application/Implementations/Notifications/FirebaseTokenProvider.cs
+++ BMM.UI.Android/Application/Implementations/Notifications/FirebaseTokenProvider.cs
@@ -1,15 +1,17 @@
 ﻿using System.Threading.Tasks;
+using Android.Gms.Extensions;
 using BMM.Core.Implementations.Notifications;
 using Firebase.Iid;
+using Firebase.Messaging;
 
 namespace BMM.UI.Droid.Application.Implementations.Notifications
 {
     public class FirebaseTokenProvider : INotificationSubscriptionTokenProvider
     {
-        // todo should replaced when the bindings are ready: https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/FirebaseMessaging#getToken()
-        public Task<string> GetToken()
+        public async Task<string> GetToken()
         {
-            return Task.FromResult(FirebaseInstanceId.Instance.Token);
+            var tokenObject = await FirebaseMessaging.Instance.GetToken();
+            return tokenObject.ToString();
         }
     }
 }
