edb9a2e528174e64370a0a079b65e766c4e657a9
Karsten Küpper <karsten.kuepper@bcc.no>
2022-08-26 11:07:07 -0400

    add event for "Streak details opened"

--- BMM.Core/GuardedActions/ExploreNewest/ListeningStreakClickedAction.cs
+++ BMM.Core/GuardedActions/ExploreNewest/ListeningStreakClickedAction.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using BMM.Api.Implementation.Models;
 using BMM.Core.GuardedActions.Base;
 using BMM.Core.GuardedActions.ExploreNewest.Interfaces;
+using BMM.Core.Implementations.Analytics;
 using BMM.Core.ViewModels;
 using BMM.Core.ViewModels.Parameters;
 using BMM.Core.ViewModels.Parameters.Interface;
@@ -14,16 +15,19 @@ namespace BMM.Core.GuardedActions.ExploreNewest
           IListeningStreakClickedAction
     {
         private readonly IMvxNavigationService _mvxNavigationService;
+        private readonly IAnalytics _analytics;
 
-        public ListeningStreakClickedAction(IMvxNavigationService mvxNavigationService)
+        public ListeningStreakClickedAction(IMvxNavigationService mvxNavigationService, IAnalytics analytics)
         {
             _mvxNavigationService = mvxNavigationService;
+            _analytics = analytics;
         }
 
         protected override async Task Execute(ListeningStreak parameter)
         {
             await _mvxNavigationService.Navigate<ListeningStreakDetailsViewModel, IListeningStreakDetailsParameter>(
                 new ListeningStreakDetailsParameter(parameter));
+            _analytics.LogEvent("Streak details opened");
         }
     }
 }
