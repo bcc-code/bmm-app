e283ad534120fce8b3ff7b9f8aea629339e009c8
Karsten Küpper <karsten.kuepper@bcc.no>
2022-12-21 11:11:13 -0500

    support other song books (which don't require mapping) (#336)

--- BMM.Api/Utils/SongbookUtils.cs
+++ BMM.Api/Utils/SongbookUtils.cs
@@ -1,20 +1,14 @@
-using System.Collections.Generic;
-
 namespace BMM.Api.Utils
 {
     public static class SongbookUtils
     {
-        private static IDictionary<string, string> NameMappingDictionary { get; } = new Dictionary<string, string>()
-        {
-            { "herrens_veier", "HV" },
-            { "mandelblomsten", "FMB" },
-        };
-
         public static string GetShortName(string name)
         {
-            return NameMappingDictionary.TryGetValue(name, out string shortName)
-                ? shortName
-                : string.Empty;
+            if (name == "herrens_veier")
+                return "HV";
+            if (name == "mandelblomsten")
+                return "FMB";
+            return name;
         }
     }
 }
